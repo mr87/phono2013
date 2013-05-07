f538e9564d793d4192c2d248f0dde1bd8c3375fa
Motiejus <motiejus.b@gmail.com>
2013-05-07 22:57:28 +0200

    init commit

---
+++ build.rb
@@ -0,0 +1,31 @@
+﻿system "compass compile --css-dir=../wp.phono.local/wp-content/themes/phono2013/stylesheets --output-style compressed --force"
+
+require 'closure-compiler'
+
+jquery = Closure::Compiler.new(:compilation_level => 'SIMPLE_OPTIMIZATIONS').compile(File.open('javascripts/vendor/jquery.js', 'r'))
+
+foundation = Closure::Compiler.new(:compilation_level => 'SIMPLE_OPTIMIZATIONS').compile_files([
+	'javascripts/foundation/foundation.js',
+	'javascripts/foundation/foundation.alerts.js',
+	'javascripts/foundation/foundation.clearing.js',
+	'javascripts/foundation/foundation.cookie.js',
+	'javascripts/foundation/foundation.dropdown.js',
+	'javascripts/foundation/foundation.forms.js',
+	'javascripts/foundation/foundation.joyride.js',
+	'javascripts/foundation/foundation.magellan.js',
+	'javascripts/foundation/foundation.orbit.js',
+	'javascripts/foundation/foundation.placeholder.js',
+	'javascripts/foundation/foundation.reveal.js',
+	'javascripts/foundation/foundation.section.js',
+	'javascripts/foundation/foundation.tooltips.js',
+	'javascripts/foundation/foundation.topbar.js'
+])
+
+app = Closure::Compiler.new(:compilation_level => 'SIMPLE_OPTIMIZATIONS').compile(File.open('javascripts/main.js', 'r'))
+
+contents = jquery + foundation + app
+
+filename = "../wp.phono.local/wp-content/themes/phono2013/javascripts/app.js"
+File.open filename, "w" do |f|
+  f.write contents
+end
---
+++ javascripts/main.js
@@ -0,0 +1,45 @@
+﻿$(document).foundation();
+$(window).on('load', function () {
+	window.scrollTo(0, 0);
+});
+
+/*
+* PANEL
+*/
+
+$('body').find('.panel-header').before('<div class="panel-header-border"/>');
+
+
+/*
+* FOOTER
+*/
+
+var footer = $('body').find('footer');
+var sticky = typeof footer.data('sticky') !== 'undefined' ? footer.data('sticky') : false;
+
+if (sticky && !Modernizr.touch) {
+	$(window).on("load resize", function () {
+		var pos = footer.position();
+		var height = $(window).height();
+		height = height - pos.top;
+		height = height - footer.height();
+		if (height > 0) {
+			footer.css({ 'margin-top': height + 'px' });
+		}
+	});
+}
+
+
+$(document).ready(function () {
+
+    /*
+    * PHOTOSWIPE
+    */
+
+    if ($('body').hasClass('gallery')) {
+        $('<div/>', {'class': 'overlay'}).appendTo('body');
+        var PS = $("#gallery a").photoSwipe({ enableMouseWheel: false, enableKeyboard: false, zIndex: 9999, fadeInSpeed:0 });
+        PS.show(0);
+    }
+
+});
